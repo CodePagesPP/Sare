183e6ee24e9774f9895c2b6f67c441e714053f5d
espinozaje <jeisson12aaron@gmail.com>
2025-08-11 15:45:12 -0500

    prueba imagenes pares

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let parts = [];
 
 let images = [
   "./assets/portada1.png",
+  "./assets/portada2.png",
   "https://images.unsplash.com/photo-1460925895917-afdab827c52f?q=80&w=1115&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "./assets/portada3.png"
 ];
