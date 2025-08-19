17a3635d547be4cd6cb17b5a43d6998fefb6cdb1
espinozaje <jeisson12aaron@gmail.com>
2025-08-19 01:11:27 -0500

    cambios 19/08

--- js/script.js
+++ js/script.js
@@ -1,10 +1,23 @@
-function openModal(img) {
+function openModal(card) {
   const modal = document.getElementById("imgModal");
   const expandedImg = document.getElementById("expandedImg");
-  expandedImg.src = img.src;
+
+
+  const bgImg = card.querySelector(".background-image");
+  expandedImg.src = bgImg.src;
+
   modal.style.display = "block";
 }
 
-function closeModal() {
+function closeModal(event) {
+  if (event) event.stopPropagation();
   document.getElementById("imgModal").style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  const modal = document.getElementById("imgModal");
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
 }
