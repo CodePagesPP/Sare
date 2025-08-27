4124553131da03d5f0e8c1ae663aac790a5125f7
PieroSanchez <pierosancas@gmail.com>
2025-08-26 22:52:27 -0500

    slider fixed

--- js/script-sliderInfo.js
+++ js/script-sliderInfo.js
@@ -3,7 +3,7 @@ const btnLeft = document.querySelector('.btn-left');
 const btnRight = document.querySelector('.btn-right');
 const card = document.querySelector('.card');
 
-const cardWidth = card.offsetWidth + 100; // ancho + margin/gap
+const cardWidth = card.offsetWidth; // ancho + margin/gap
 
 btnRight.addEventListener('click', () => {
   slider.scrollBy({ left: cardWidth, behavior: 'smooth' });
